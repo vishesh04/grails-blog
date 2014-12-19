a3c7cab2c2b549f1352494540b0713bcce9d4595
vishesh <vishesh.singhal@eltropy.com>
2014-12-19 23:32:35 +0530

    Fixed topcoder project import api

--- grails-app/controllers/content/ProjectController.groovy
+++ grails-app/controllers/content/ProjectController.groovy
@@ -26,69 +26,71 @@ class ProjectController {
     }
 
     def importFromTc() {
-        def resp = [:]
-        def projects
-        if (request.method == "GET") {
-            def restClient = new RESTClient(TC_API)
-            try {
-                def authResponse = restClient.post(path: '/v2/auth',
-                        body: [username: TC_HANDLE,
-                        password: params.tcPassword],
-                        requestContentType: URLENC
-                )
-                def jwtToken = authResponse.data.token
-                log.info("importing tc projects. jwt $jwtToken")
-                restClient.headers."Authorization" = "Bearer $jwtToken"
-                def apiResponse = restClient.get(path: '/v2/user/challenges',
-                        query: [type: 'PAST'])
-                projects = apiResponse.data.data
-                resp.success = true
-            } catch (e) {
-                log.error(e)
-                resp.success = false
-                resp.message = "Please try with manual json"
-            }
-        } else {
-            projects = request.JSON.data
-            resp.success = true
-        }
-        projects.each {
-            def project = Project.findByTcChallengeId(it.challengeId)
-            if (!project) {
-                try {
-                    if (it.status == "Completed") {
-                        def resultClient = new RESTClient(TC_API)
-                        def resultResponse = resultClient.get(path: "/v2/develop/challenges/result/$it.challengeId")
-                        def results = resultResponse.data.results
-                        //check if I won/submitted, if so create project in db
-                        if (results.handle.contains(TC_HANDLE)) {
-                            log.info("creating project with title: $it.challengeName")
-                            // get the challenge detail
-                            def challengeClient = new RESTClient(TC_API)
-                            def challengeResponse = challengeClient.get(path: "/v2/challenges/$it.challengeId")
-                            def challenge = challengeResponse.data
-                            project = new Project()
-                            project.title = it.challengeName
-                            project.platforms = it.platforms
-                            project.technologies = it.technologies
-                            project.tcChallengeId = it.challengeId
-                            project.url = "$TC_SITE/challenge-details/$it.challengeId"
-                            def length = challenge.detailedRequirements?.size()
-                            if (length && length > 310) {
-                                length = 310
+        def requestMethod = request.method
+        def tcPassword = params.tcPassword
+        Project.async.task {
+            withTransaction {
+                def projects
+                if (requestMethod == "GET") {
+                    def restClient = new RESTClient(TC_API)
+                    try {
+                        def authResponse = restClient.post(path: '/v2/auth',
+                                body: [username: TC_HANDLE,
+                                       password: tcPassword],
+                                requestContentType: URLENC
+                        )
+                        def jwtToken = authResponse.data.token
+                        log.info("importing tc projects. jwt $jwtToken")
+                        restClient.headers."Authorization" = "Bearer $jwtToken"
+                        def apiResponse = restClient.get(path: '/v2/user/challenges',
+                                query: [type: 'PAST'])
+                        projects = apiResponse.data.data
+                    } catch (e) {
+                        log.error(e)
+                    }
+                } else {
+                    projects = request.JSON.data
+                }
+                projects.each {
+                    def project = Project.findByTcChallengeId(it.challengeId)
+                    if (!project) {
+                        try {
+                            if (it.status == "Completed") {
+                                def resultClient = new RESTClient(TC_API)
+                                def resultResponse = resultClient.get(path: "/v2/develop/challenges/result/$it.challengeId")
+                                def results = resultResponse.data.results
+                                //check if I won/submitted, if so create project in db
+                                if (results.handle.contains(TC_HANDLE)) {
+                                    log.info("creating project with title: $it.challengeName")
+                                    // get the challenge detail
+                                    def challengeClient = new RESTClient(TC_API)
+                                    def challengeResponse = challengeClient.get(path: "/v2/challenges/$it.challengeId")
+                                    def challenge = challengeResponse.data
+                                    project = new Project()
+                                    project.title = it.challengeName
+                                    project.platforms = it.platforms
+                                    project.technologies = it.technologies
+                                    project.tcChallengeId = it.challengeId
+                                    project.url = "$TC_SITE/challenge-details/$it.challengeId"
+                                    project.projectOrder = 9999 // it will be replaced with appropriate value by trigger
+                                    def length = challenge.detailedRequirements?.size()
+                                    if (length && length > 310) {
+                                        length = 310
+                                    }
+                                    project.description = challenge.detailedRequirements?.substring(0,length)
+                                            .replaceAll("<(.|\n)*?>", '')+"<a target='_blank' href=$project.url>...View more</a>"
+                                    project.save(flush: true, failOnError: true)
+                                }
                             }
-                            project.description = challenge.detailedRequirements?.substring(0,length)
-                                    .replaceAll("<(.|\n)*?>", '')+"<a target='_blank' href=$project.url>...View more</a>"
-                            project.save(flush: true)
+                        } catch (e) {
+                            log.error "error importing project. title: $it.challengeName. id: $it.challengeId", e
                         }
                     }
-                } catch (e) {
-                    log.error "error importing project. title: $it.challengeName. id: $it.challengeId"
                 }
+                log.info("Done importing")
             }
         }
-        log.info("Done importing")
-        resp.projets = Project.findAll()
+        def resp = [started: true]
         render resp as JSON
     }
 
