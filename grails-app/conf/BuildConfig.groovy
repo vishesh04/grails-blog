92ec130f22b7c5a6451845c8def9691850032929
Vishesh Singhal <vishesh.singhal91@gmail.com>
2019-07-19 16:03:15 +0530

    Added maven repos

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,6 +41,8 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
+        mavenRepo "http://repo.grails.org/grails/plugins"
+        mavenRepo "http://repo1.maven.org/maven2"
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
